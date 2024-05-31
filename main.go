62e89d44b6172fffd010105701401265d3e44abe
Tairo Forbat Araujo <forbatnew@gmail.com>
2024-05-31 08:53:15 -0400

    feat: add logger with schema and custom , and add config custom

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/forbatnew/gopportunities/router"
+import (
+	"github.com/forbatnew/gopportunities/config"
+	"github.com/forbatnew/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	//Initialize configuration
+	err := config.Init()
+
+	if err != nil {
+		//	panic(err)
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	//Initialize router
 	router.Initialize()
 }
