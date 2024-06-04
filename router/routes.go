8fc80ce5b0b277a42f0bb84328edec49ec3847d6
Tairo Forbat Araujo <forbatnew@gmail.com>
2024-06-03 22:07:03 -0400

    feat: add crud and configurations, and start creating documentation with swagger

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,18 @@
 package router
 
 import (
+	"github.com/forbatnew/gopportunities/docs"
 	"github.com/forbatnew/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	//Initialize Handler
 	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	v1 := router.Group("/api/v1")
 	{
 		//Show Opening
@@ -17,5 +22,9 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
 	}
+
+	//Initialize Swagger
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 }
